b08d8bbed1561bd4bc058bf84bf823cdf9445f01
zhengdongyi98 <57354065+zhengdongyi98@users.noreply.github.com>
2021-04-11 16:01:06 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <container></container>
   </div>
 </template>
-
+<script>
+  import container from "./components/content/container"
+  export default {
+    name: 'App',
+    components: {
+      container
+    }
+  }
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  @import "assets/css/base.css";
+  html,body,#app{
+    height:100%;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+  }
 </style>
